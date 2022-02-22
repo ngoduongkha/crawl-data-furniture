b8daa92639619c015d80b179664b7cd355022d26
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-02-22 09:55:04 +0700

    .

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function main() {
   const browser = await puppeter.launch();
   const page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: 'load', timeout: 0 });
+  await page.goto(url);
 
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
@@ -41,7 +41,7 @@ async function main() {
       )
     );
 
-    const srcSetAttribute = items.slice(0, 1).map((item) => {
+    const srcSetAttribute = items.slice(0, 20).map((item) => {
       const gethref = item.querySelector('a').getAttribute('href');
       return gethref;
     });
@@ -51,30 +51,45 @@ async function main() {
 
   console.log(links);
 
+  const database = await Promise.all(
+    links.map(async (link) => {
+      const page = await browser.newPage();
+      await page.goto(link, { waitUntil: 'load', timeout: 0 });
+
+      const data = await page.evaluate(() => {
+        const title = document.querySelector('h1.product-title').innerText;
+        const price = String(
+          document.querySelector('.woocommerce-Price-amount.amount').innerText
+        );
+        const size = document.querySelector(
+          'div.product-attribute.product-attribute-item.product-attribute-item-0 span.product-attribute-option'
+        ).innerText;
+        const productBaseId = document.querySelector('.sku').innerText;
+        const detailMaterial = Array.from(
+          document.querySelectorAll(
+            'div.product-attribute.product-attribute-item.product-attribute-item-0 span.product-attribute-option'
+          )
+        ).map((e) => e.innerText);
+        detailMaterial.shift();
+        return {
+          ProductBasetId: productBaseId,
+          CategoryId: 5,
+          BrandId: 1,
+          MaterialId: 1,
+          Name: title,
+          Size: size,
+          Description: '',
+          Quantity: 0,
+          Price: price,
+          Image: '',
+          detailMaterial: detailMaterial,
+        };
+      });
+      return data;
+    })
+  );
+
   await browser.close();
-
-  const database = await links.map(async (link) => {
-    const browser = await puppeter.launch();
-    const page = await browser.newPage();
-    await page.goto(link, { waitUntil: 'load' });
-
-    console.log('aaaa');
-
-    const data = await page.evaluate(() => {
-      const title = document.querySelector('h1.product-title').innerText;
-      const price = document.querySelector(
-        '.woocommerce-Price-amount.amount'
-      ).innerText;
-      return {
-        Name: title,
-        Price: price,
-      };
-    });
-
-    return data;
-  });
-  await browser.close();
-
   console.log(database);
 }
 main();
